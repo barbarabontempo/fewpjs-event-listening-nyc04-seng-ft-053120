d77bf41509e296bd4331f8912718b4cba2ab6107
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-08-03 16:36:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
-    input.addEventListener('click', function(event) {
-    alert('I was clicked!');
-    });
+    const input = document.getElementById('input'); //finding the elemeny via id name
+    input.addEventListener('click', function(event) { //calling variable input that we made above and calling addEventListener() on that variable
+    alert('I was clicked!'); //addEventListener takes in two arguments: name of event and a callback function to "handle" event
+    }); //
 }
